b65e7c6c6ab9330a42be00f3b5a66aa8ee2dd89f
Martin Davis <martin_davis3@yahoo.ca>
2020-09-11 16:53:02 -0600

    Updates

---
+++ precache-manifest.c18748a59df631dc70590a1b104de837.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3664f018b1e57aa3abb80ad449deb1b",
+    "url": "/React-Redux-ToDo-List/index.html"
+  },
+  {
+    "revision": "8f57485ab1a3b7277b7e",
+    "url": "/React-Redux-ToDo-List/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "ca6963fc726ae8754f6a",
+    "url": "/React-Redux-ToDo-List/static/js/2.b1c3433e.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/React-Redux-ToDo-List/static/js/2.b1c3433e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f57485ab1a3b7277b7e",
+    "url": "/React-Redux-ToDo-List/static/js/main.9d5531aa.chunk.js"
+  },
+  {
+    "revision": "7bd55196e4c7d790142d",
+    "url": "/React-Redux-ToDo-List/static/js/runtime-main.82e49470.js"
+  }
+]);
